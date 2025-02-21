fd5d2387d2fbdd610fade5b61cb816f9c860c7d6
kyle-233 <lubingfu@chinabzc.com>
2025-02-21 16:33:31 +0800

    feat: add i18n

---
+++ src/i18n/config.ts
@@ -0,0 +1,30 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import languageDetector from 'i18next-browser-languagedetector'
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: {
+      edit: 'Welcome to React and react-i18next',
+    },
+  },
+  zh: {
+    translation: {
+      edit: 'Bienvenue à React et react-i18next',
+    },
+  },
+}
+
+i18n
+  .use(languageDetector)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
+
+export default i18n
