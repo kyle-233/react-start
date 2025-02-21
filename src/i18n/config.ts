af926125f5940aba2bb263f4ad34c27941fd49f9
kyle-233 <lubingfu@chinabzc.com>
2025-02-21 17:29:11 +0800

    feat: splice language to locales

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,30 +1,22 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import languageDetector from 'i18next-browser-languagedetector'
+import httpBackend from 'i18next-http-backend'
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  en: {
-    translation: {
-      edit: 'Welcome to React and react-i18next',
-    },
-  },
-  zh: {
-    translation: {
-      edit: 'Bienvenue à React et react-i18next',
-    },
-  },
-}
 
 export const supportedLngs = ['en', 'zh']
 
 i18n
+  .use(httpBackend)
   .use(languageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    backend: {
+      loadPath: '/src/i18n/locales/{{lng}}/{{ns}}.json',
+    },
     supportedLngs,
-    resources,
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
