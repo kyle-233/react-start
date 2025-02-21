b75e4eb0825e660d4e431bf491ebbe35dba3a2db
kyle-233 <lubingfu@chinabzc.com>
2025-02-21 17:44:29 +0800

    feat: fix translate bug while build

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,6 +2,8 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import languageDetector from 'i18next-browser-languagedetector'
 import httpBackend from 'i18next-http-backend'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import chainedBackend from 'i18next-chained-backend'
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -9,12 +11,23 @@ import httpBackend from 'i18next-http-backend'
 export const supportedLngs = ['en', 'zh']
 
 i18n
-  .use(httpBackend)
   .use(languageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(chainedBackend)
   .init({
     backend: {
-      loadPath: '/src/i18n/locales/{{lng}}/{{ns}}.json',
+      backends: [
+        httpBackend,
+        resourcesToBackend((language: string, namespace: string) => {
+          if (language === 'dev') return
+          return import(`./locales/${language}/${namespace}.json`)
+        }),
+      ],
+      backendOptions: [
+        {
+          loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+      ],
     },
     supportedLngs,
     interpolation: {
