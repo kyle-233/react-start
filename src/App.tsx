04679248320aa20022aeb4ebeee4fe0d0ce0cce1
kyle-233 <kyle07142113@163.com>
2025-02-22 22:53:24 +0800

    feat: add light and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import { useTranslation } from 'react-i18next'
 import { LngSelect } from '@/components/lng-select'
 import { Button } from '@/components/ui/button'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
 
 function App() {
   const { t } = useTranslation('translation')
   return (
-    <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <LngSelect />
       {t('edit')}
       <Button>Click me</Button>
-    </div>
+      <ModeToggle />
+    </ThemeProvider>
   )
 }
 
