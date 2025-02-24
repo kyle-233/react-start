9461826f67c534e330fadf6b99873c922f1f6547
kyle-233 <lubingfu@chinabzc.com>
2025-02-24 17:55:20 +0800

    feat: water page initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import { useTranslation } from 'react-i18next'
-import { LngSelect } from './components/lng-select'
+import { Hero } from './components/Hero/hero'
 
 function App() {
-  const { t } = useTranslation('translation')
   return (
-    <div>
-      <LngSelect />
-      {t('edit')}
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
