62ed1346a90b91c1f9bff14e07f0305d8851ae7b
kyle-233 <kyle07142113@163.com>
2025-03-02 00:46:20 +0800

    feat: add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    reporters: [['verbose', { summary: false }]],
     environment: 'happy-dom',
     // setupFiles: '.vitest/setup',
     coverage: {
